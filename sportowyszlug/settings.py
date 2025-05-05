9000aa3a0738c55fbebcb9c754967f73de001470
gufi2115 <148818468+gufi2115@users.noreply.github.com>
2025-05-05 12:06:25 +0200

    Update settings.py

--- sportowyszlug/settings.py
+++ sportowyszlug/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sportowyszlug',
         'USER': 'postgres',
-        'PASSWORD': 'buber2006',
+        'PASSWORD': 'xxxx',
         'HOST': 'localhost',
         'PORT': '5432',
     }
